3e0e4a9f479ef3cc9f086da3d8e90a00483e54dc
grush <grush@DESKTOP-JFK81GP>
2024-06-26 00:02:59 +0300

    Добавьте файлы проекта.

---
+++ lab12/lab12.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+
+const int rows = 10;
+const int cols = 15;
+
+
+int findMaxElement(int arr[], int size) {
+    int maxElement = arr[0];
+    for (int i = 1; i < size; ++i) {
+        if (arr[i] > maxElement) {
+            maxElement = arr[i];
+        }
+    }
+    return maxElement;
+}
+
+int main() {
+
+    int array[rows][cols] = {
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
+        {3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17},
+        {4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18},
+        {5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19},
+        {6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+        {7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21},
+        {8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22},
+        {9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23}
+    };
+
+
+    int rowSums[rows] = { 0 };
+
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            rowSums[i] += array[i][j];
+        }
+    }
+
+
+    std::cout << "Sum of elements in each row:" << std::endl;
+    for (int i = 0; i < rows; ++i) {
+        std::cout << "Row " << i << ": " << rowSums[i] << std::endl;
+    }
+
+
+    int maxElement = findMaxElement(rowSums, rows);
+
+
+    std::cout << "Maximum sum of elements in rows: " << maxElement << std::endl;
+
+    return 0;
+}
